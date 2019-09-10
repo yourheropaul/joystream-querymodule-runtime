fd522776525a949dd5c0e42a477b300291667530
Paul M Fox <iam@yourheropaul.com>
2019-09-10 12:15:21 +0100

    Removed old files; unit test support

---
+++ src/__tests__/example.spec.ts
@@ -0,0 +1,7 @@
+import { Plain } from "../lib/joystream/query/storage"
+
+describe("example", () => {
+    it("should be X", () => {
+        expect<i32>(19 + 23).toBe(42, "19 + 23 is 42")
+    })
+})
